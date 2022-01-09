99c4e29ca4352dd3fb3eb239af71cc4fe41a4f99
Diego Salas <dsalas035@gmail.com>
2022-01-09 11:28:57 -0600

    redirects added

--- src/config/google.js
+++ src/config/google.js
@@ -3,15 +3,21 @@ import { GOOGLE_ANALYTICS_TRACK_ID } from '../constants/secrets';
 
 // Initialize Google Analytics
 export const initializeGA = () => {
-  ReactGA.initialize(GOOGLE_ANALYTICS_TRACK_ID);
+  if (process.env.NODE_ENV === 'production') {
+    ReactGA.initialize(GOOGLE_ANALYTICS_TRACK_ID);
+  }
 };
 
 // Send pageview with a custom path
 export const gaSendPageview = (path) => {
-  ReactGA.send({ hitType: 'pageview', page: path });
+  if (process.env.NODE_ENV === 'production') {
+    ReactGA.send({ hitType: 'pageview', page: path });
+  }
 };
 
 // Send a custom event
 export const gaSendEvent = ({ category, action }) => {
-  ReactGA.event({ category, action });
+  if (process.env.NODE_ENV === 'production') {
+    ReactGA.event({ category, action });
+  }
 };
